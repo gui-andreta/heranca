6e6d05feb9506383b5568f998655135734fc1df1
Guilherme Andreta <guilherme.andretasantos@hotmail.com>
2023-04-17 10:38:26 -0300

    first commit

---
+++ HERANCA/Program.cs
@@ -0,0 +1,57 @@
+﻿using PESSOA;
+
+internal class Program {
+    private static void Main(string[] args) {
+
+        //Instanciando e Chamento classe Pai
+        Console.Write("Instancie uma Pessoa: \n\nNome: ");
+        string nome = Console.ReadLine();
+
+        Console.Write("Rg: ");
+        string rg = Console.ReadLine();
+
+        Console.Write("CPF: ");
+        string cpf = Console.ReadLine();
+
+        Console.Write("Data de Nascimento: ");
+        DateTime nasc = DateTime.Parse(Console.ReadLine());
+
+        //Instaciando Pessoa e chamando Metodo
+        Pessoa p = new Pessoa(nome, rg, cpf, nasc);
+        Console.WriteLine("Pessoa Instanceada");
+        Console.WriteLine(p.Identificacao());
+
+        //Instanciando Aluno e chamando metodo Herdado
+        Console.Write("Instancie um Aluno: \n\nRA: ");
+        string ra = Console.ReadLine();
+
+        Console.Write("Curso: ");
+        string curso = Console.ReadLine();
+
+        Console.Write("Mensalidade: R$ ");
+        double mensalidade = double.Parse(Console.ReadLine());
+
+        //Instanciando e chamando Metodo
+        Pessoa a = new Aluno(ra, curso, mensalidade);
+        Console.WriteLine("Aluno Instanciado \n");
+        Console.WriteLine(a.Identificacao());
+
+        //Instanciando Professor e chamando metodo Herdado
+        Console.Write("Instancie um Professor: \n\nFuncional: ");
+        string funcional = Console.ReadLine();
+
+        Console.Write("Disciplina: ");
+        string disciplina = Console.ReadLine();
+
+        Console.Write("Salario: R$ ");
+        double salario = double.Parse(Console.ReadLine());
+
+        //Instanciando e chamando Metodo
+        Pessoa prof = new Professor(funcional, disciplina, salario);
+        Console.WriteLine("Professor Instanciado \n");
+        Console.WriteLine(a.Identificacao());
+
+        //Fim do Programa
+        Console.WriteLine("Fim do Programa");
+    }
+}
---
+++ PESSOA/Class1.cs
@@ -0,0 +1,61 @@
+﻿namespace PESSOA {
+    public class Pessoa {
+        private string Nome { get; set; }
+        private string Rg { get; set; }
+        private string Cpf { get; set; }
+        private DateTime Nasc { get; set; }
+
+        //Construtores
+        public Pessoa() {
+
+        }
+
+        public Pessoa(string nome, string rg, string cpf, DateTime nasc) {
+            Nome = nome;
+            Rg = rg;
+            Cpf = cpf;
+            Nasc = nasc;
+        }
+
+        //Function
+        public virtual string Identificacao() {
+            return Cpf;
+        }
+    }
+
+    public class Professor : Pessoa {
+        public string Funcional { get; private set; }
+        public string Disciplina { get; private set; }
+        public double Salario { get; private set; }
+
+        //Contrutores
+        public Professor(string func, string curso, double valor) : base() { 
+            Funcional = func;
+            Disciplina = curso;
+            Salario = valor;
+        }
+
+        //Override
+        public override string Identificacao() {
+            return Funcional;
+        }
+    }
+
+    public class Aluno : Pessoa {
+        public string Ra { get; private set; }
+        public string Curso { get; private set; }
+        public double Mensalidade { get; private set; }
+
+        //Contrutor
+        public Aluno(string ra, string curso, double valor) : base() {
+            Ra = ra;
+            Curso = curso;
+            Mensalidade = valor;
+        }
+
+        //Override
+        public override string Identificacao() {
+            return Ra;
+        }
+    }
+}
